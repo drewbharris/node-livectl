76a24bffebf876dd36b292fded2554d1d324bf63
Drew Harris <contac@grmnygrmny.com>
2012-11-18 17:05:42 -0500

    added packaging

---
+++ package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+basedir="./";
+
+TARDIR="./dist"
+
+mkdir -p ${TARDIR};
+cp -Rpa ${TARDIR}/AppBundle.skel.app ${TARDIR}/livectl.app;
+cp -Rpa ${basedir}/lib/appjs/bin/* ${TARDIR}/livectl.app/Contents/MacOS/bin/;
+cp -Rpa ${basedir}/node_modules ${TARDIR}/livectl.app/Contents/Resources/;
+cp -Rpa ${basedir}/lib/appjs/content ${TARDIR}/livectl.app/Contents/Resources/;
+cp -Rpa ${basedir}/lib ${TARDIR}/livectl.app/Contents/Resources/;
+cp -Rpa ${basedir}/index.js ${TARDIR}/livectl.app/Contents/Resources/app.js;
