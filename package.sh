6d34347be10c6acd792531fe606956d9ebfdaae6
Drew Harris <contact@grmnygrmny.com>
2012-11-18 17:08:15 -0500

    fixing

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@ TARDIR="./dist"
 
 mkdir -p ${TARDIR};
 cp -Rpa ${TARDIR}/AppBundle.skel.app ${TARDIR}/livectl.app;
-cp -Rpa ${basedir}/lib/appjs/bin/* ${TARDIR}/livectl.app/Contents/MacOS/bin/;
+cp -Rpa ${basedir}/lib/appjs/bin ${TARDIR}/livectl.app/Contents/MacOS/;
 cp -Rpa ${basedir}/node_modules ${TARDIR}/livectl.app/Contents/Resources/;
 cp -Rpa ${basedir}/lib/appjs/content ${TARDIR}/livectl.app/Contents/Resources/;
 cp -Rpa ${basedir}/lib ${TARDIR}/livectl.app/Contents/Resources/;
